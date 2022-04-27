0758355b4e0e482ce5125004f44481297e8f92c6
YammyToast <cyanjamesmail@gmail.com>
2022-04-27 15:47:14 +0100

    Add project files.

---
+++ CMP1903M - Object Oriented Programming 2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace DiceGame {
+    class Program {
+
+        public static void Main(string[] args) {
+            Console.WriteLine("Test");
+        }
+    }
+}
